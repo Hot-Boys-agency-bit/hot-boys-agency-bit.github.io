2aae9c32406cdadf6900804858a10f9f0a03cec6
Hot-Boys-agency-bit <58664713+Hot-Boys-agency-bit@users.noreply.github.com>
2019-12-16 15:37:56 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,7 +1,12 @@
 <?php
  $name = $_POST['name'];
  $email = $_POST['email'];
- 
+ $email = $_POST['email'];
+ $email = $_POST['email'];
+ $email = $_POST['email'];
+
+
+
  $subject = "=?utf-8?B?".base64_encode("Сообщение с сайта")."?=";
  $headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/html; charset=utf-8\r\n";
 
