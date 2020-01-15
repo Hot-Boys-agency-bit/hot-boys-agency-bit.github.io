b391d93530e3036450b9be9ba9a0081ae05b4258
Hot-Boys-agency-bit <58664713+Hot-Boys-agency-bit@users.noreply.github.com>
2020-01-15 15:22:38 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -23,7 +23,7 @@ if ( $method === 'POST' ) {
 } else if ( $method === 'GET' ) {
 
 	$project_name = trim($_GET["project_name"]);
-	$admin_email  = "anton.bukashin@yandex.ru"; // кому
+	$admin_email  = ""; // кому
 	$form_subject = trim($_GET["form_subject"]);
 
 	foreach ( $_GET as $key => $value ) {
